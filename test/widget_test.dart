b7d5c0fe81bafd21a1ef2aa959be9c40a2cb2452
Mustapha <mustapha.aouadi2@gmail.com>
2024-08-27 02:17:24 +0300

    122

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,6 +13,7 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
+    debugPrint(tester.element(find.byType(MyApp)).toString());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
