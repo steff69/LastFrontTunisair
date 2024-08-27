0c2951edb04a7e4bfbddcd474f7fe485f3d2f832
Mustapha <mustapha.aouadi2@gmail.com>
2024-08-27 04:40:47 +0300

    1223

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,21 +10,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:travel_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-    debugPrint(tester.element(find.byType(MyApp)).toString());
+ testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  // Build the widget
+  await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  // Verify that the counter starts at 0
+  expect(find.text('0'), findsOneWidget);
+  expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  // Tap the '+' icon and trigger a frame
+  await tester.tap(find.byIcon(Icons.add));
+  await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+  // Verify that the counter has incremented
+  expect(find.text('0'), findsNothing);
+  expect(find.text('1'), findsOneWidget);
+});
+
