e6145eee944812f870615ac41714abb59573c942
Mustapha <mustapha.aouadi2@gmail.com>
2024-08-28 17:49:11 +0300

    1223

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tunisair_master/constants/constants.dart'; 
-import 'package:tunisair_master/lib/pages/MainScreen.dart';
-import 'package:tunisair_master/lib/pages/welcome_page.dart.dart';
+import 'package:tunisair_master/pages/MainScreen.dart';
+import 'package:tunisair_master/pages/welcome_page.dart';  // Corrected import for welcome_page.dart
 
 void main() {
   runApp(const MyApp());
@@ -14,20 +14,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(375, 825),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        // Use builder only if you need to use library outside ScreenUtilInit context
-        builder: (context, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'fudly',
-            theme: ThemeData(
-              scaffoldBackgroundColor: kOffwhite,
-              iconTheme: IconThemeData(color: kDark),
-            ),
-            home: const WelcomePage(),
-          );
-        });
+      designSize: const Size(375, 825),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'fudly',
+          theme: ThemeData(
+            scaffoldBackgroundColor: kOffwhite,
+            iconTheme: IconThemeData(color: kDark),
+          ),
+          home: const WelcomePage(),  // This should correctly load your WelcomePage
+        );
+      },
+    );
   }
 }
